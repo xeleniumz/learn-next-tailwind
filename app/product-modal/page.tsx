c3b166ce1d0f667e0c8253ce5c082a1facce8387
Xeleniumz Fx <demolismz@windowslive.com>
2024-05-24 16:25:27 +0700

    add dev

---
+++ app/product-modal/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const page = () => {
+    return (
+    //   global container
+        <div className="flex items-center justify-center  min-h-screen
+                bg-slate-100
+        ">
+        {/* card container */}
+        <div className="flex flex-col my-6 space-y-6 
+              md:space-y-0 md:space-x-6 md:flex-row md:my-y-0
+        ">
+            <div className="flex flex-col items-center justify-center
+                w-80 h-80 p-6 bg-white shadow-lg rounded-lg
+            ">
+                <div className="flex flex-col">
+                      
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
